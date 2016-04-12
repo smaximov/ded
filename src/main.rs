2c67894b26dbce6bef632cd7f8458c345d3c2db7
smaximov <s.b.maximov@gmail.com>
2016-04-12 21:27:57 +1000

    feat: add option to set working directory

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::io;
 use std::io::{Write};
+use std::path::{PathBuf};
+use std::process::{exit};
 
 extern crate clap;
 extern crate crypto;
 extern crate eventual;
 extern crate sequence_trie;
+
+use clap::{Arg};
 
 pub mod app;
 pub mod entry;
@@ -26,15 +30,34 @@ fn get_editor() -> String {
 }
 
 fn main() {
-    let current_dir = env::current_dir()
-        .expect("cannot get current working directory");
+    let matches = clap::App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .arg(Arg::with_name("WORKING_DIR")
+             .index(1)
+             .help("working directory [defaults to $PWD]"))
+        .get_matches();
 
     let editor = get_editor();
+
+    let working_dir = matches.value_of("WORKING_DIR")
+        .map(|dir| PathBuf::from(dir).canonicalize())
+        .unwrap_or(env::current_dir());
+
+    let working_dir = match working_dir {
+        Ok(dir) => dir,
+        Err(e) => {
+            let mut stderr = io::stderr();
+            writeln!(stderr, "Cannot resolve working directory: {}", e).unwrap();
+            exit(1);
+        }
+    };
 
     let path = temp_dir(TMP_PREFIX)
         .expect("cannot create temporary directory");
 
-    let config = Config::new(&current_dir, &path, &editor);
+    let config = Config::new(&working_dir, &path, &editor);
     let mut app = App::new(config);
 
     if let Err(e) = app.run() {
